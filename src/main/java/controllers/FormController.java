6dcdec80225e92134fb8b1201b77c42a6eba5ba3
Eliot Godard <eliot.godard@telecomnancy.net>
2016-12-13 22:33:32 +0100

    prepared SQL request

--- src/main/java/controllers/FormController.java
+++ src/main/java/controllers/FormController.java
@@ -20,7 +20,8 @@ public class FormController extends Controller {
     private HBox buttonsContainer;
 
     private Question curQuestion;
-    private HashMap<String, String> flags;
+    private HashMap<Flag, String> flags;
+    private String request;
 
     @FXML
     private void initialize() {
@@ -28,28 +29,28 @@ public class FormController extends Controller {
     	//       PRESETS       //
     	/////////////////////////
     	Question q1 = new Question("Que voulez-vous faire ?");
-    	Question q2 = new Question("Quel genre ?", "TYPE");
-    	Question q3 = new Question("Quel domaine ?", "TYPE");
-    	Question q4 = new Question("Combien de temps avez-vous ?", "TEMPS");
+    	Question q2 = new Question("Quel genre ?", Flag.TYPE);
+    	Question q3 = new Question("Quel domaine ?", Flag.TYPE);
+    	Question q4 = new Question("Combien de temps avez-vous ?", Flag.TEMPS);
 
     	Answer a1_1 = new Answer("Me divertir", q2);
     	Answer a1_2 = new Answer("M'instruire", q3);
     	q1.addAnswer(a1_1);
     	q1.addAnswer(a1_2);
     	
-    	Answer a2_1 = new Answer("Jeux-vidéo", q4);
-    	Answer a2_2 = new Answer("Sport", q4);
+    	Answer a2_1 = new Answer("Jeux-vidéo", "jv", q4);
+    	Answer a2_2 = new Answer("Sport", "sport", q4);
     	q2.addAnswer(a2_1);
     	q2.addAnswer(a2_2);
     	
-    	Answer a3_1 = new Answer("Sciences", q4);
-    	Answer a3_2 = new Answer("Histoire", q4);
+    	Answer a3_1 = new Answer("Sciences", "sciences", q4);
+    	Answer a3_2 = new Answer("Histoire", "histoire", q4);
     	q3.addAnswer(a3_1);
     	q3.addAnswer(a3_2);
     	
-    	Answer a4_1 = new Answer("5 minutes");
-    	Answer a4_2 = new Answer("10 minutes");
-    	Answer a4_3 = new Answer("30 minutes");
+    	Answer a4_1 = new Answer("5 minutes", "temps < 6");
+    	Answer a4_2 = new Answer("10 minutes", "temps >= 6 && temps < 11");
+    	Answer a4_3 = new Answer("30 minutes", "temps >= 11");
     	q4.addAnswer(a4_1);
     	q4.addAnswer(a4_2);
     	q4.addAnswer(a4_3);
@@ -59,7 +60,7 @@ public class FormController extends Controller {
     	
     	update();
     	
-    	flags = new HashMap<String, String>();
+    	flags = new HashMap<Flag, String>();
     	
     	/*
     	this.searchButton.setOnMouseReleased(new EventHandler<MouseEvent>() {
@@ -69,6 +70,25 @@ public class FormController extends Controller {
             }
         });
         */
+    }
+    
+    private void generateRequest(){
+    	String Request = "";
+    	String WHERE = "";
+    	for(Flag s : this.flags.keySet()){
+    		if(!WHERE.equals(""))
+        		WHERE += "&& ";
+		
+			if(s == Flag.TYPE)
+				WHERE += "type == " + this.flags.get(s)+" ";
+			if(s == Flag.TEMPS)
+    			WHERE += this.flags.get(s)+" ";
+    	}
+    	
+    	Request += "SELECT * IN VIDEO_SUGGESTION ";
+    	Request += "WHERE " + WHERE;
+    	
+    	System.out.println(Request);
     }
     
     private void update(){
@@ -81,7 +101,7 @@ public class FormController extends Controller {
 	                @Override
 	                public void handle(MouseEvent event) {
 	                	if(curQuestion.isMemorised())
-	                		flags.put(curQuestion.getFlag(), curAnswer.getText());
+	                		flags.put(curQuestion.getFlag(), curAnswer.getRealValue());
 	                	
 	                    curQuestion = curButton.getAnswer().getLeadingQuestion();
 	                    buttonsContainer.getChildren().clear();
@@ -94,6 +114,12 @@ public class FormController extends Controller {
     	else{
     		this.questionLabel.setText("DONE");
     		System.out.println(this.flags.toString());
+        	generateRequest();
     	}
+    }
+    
+    public enum Flag{
+    	TYPE,
+    	TEMPS;
     }
 }
