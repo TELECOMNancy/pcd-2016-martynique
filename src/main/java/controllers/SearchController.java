ba12fc9b233875ebc4d1966a56d7024fde376b8e
Mcdostone <yann.prono@telecomnancy.net>
2016-12-13 18:42:29 +0100

    OK

--- src/main/java/controllers/SearchController.java
+++ src/main/java/controllers/SearchController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import models.Search;
+import utils.Search;
 
 /**
  * Controller for searching videos on Youtube
