0a8e0b75c90170d2b53c47c36927cb376ce98884
Mcdostone <yann.prono@telecomnancy.net>
2016-12-14 11:54:52 +0100

    Favorites soon finished

---
+++ src/main/java/controllers/YoutubeTabPaneController.java
@@ -0,0 +1,52 @@
+package controllers;
+
+import app.SceneManager;
+import db.FavoriteDB;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Tab;
+import models.Favorite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Controller managing all tabs on application
+ */
+public class YoutubeTabPaneController extends Controller {
+
+    @FXML
+    private Tab favoritesTab;
+    @FXML
+    private Tab playlistTab;
+
+    public YoutubeTabPaneController() {
+        super();
+    }
+
+    @FXML
+    public void initialize() {
+        this.favoritesTab.setOnSelectionChanged(new EventHandler<Event>() {
+            @Override
+            public void handle(Event event) {
+                if(favoritesTab.isSelected())
+                    showFavorites();
+            }
+        });
+    }
+
+    private void showFavorites() {
+        List<Favorite> list = new ArrayList<Favorite>();
+        FavoriteDB db = new FavoriteDB(null);
+        list = db.all();
+
+        FXMLLoader loader = SceneManager.getLoader("results.fxml");
+        FavoritesController ctrl = new FavoritesController(list);
+        ctrl.injectAppController(this.appController);
+        loader.setController(ctrl);
+        this.favoritesTab.setContent(SceneManager.getComponent(loader));
+    }
+
+}
